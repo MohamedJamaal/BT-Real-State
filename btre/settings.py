449496e00f95605f4a41d64703df9e0e5d7b1311
MohamedJamaal <mohamed_gamal19@hotmail.com>
2019-12-18 14:55:16 +0200

    custmizing dashboard and inquiriers

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR : 'danger'
 }
+
+## Email Configuration Gmail 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='mg69126@gmail.com'
+EMAIL_HOST_PASSWORD ='gemy2244668899'
+EMAIL_USE_TLS =True
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def contact(request):
-    return
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+        listing = request.POST['listing']
+        listing_id = request.POST['listing_id']
+
+        ## Making user can NOT make inquery for same home twice , its just once for user for home 
+        if request.user.is_authenticated :
+            user_id = request.user.id
+            has_connected = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_connected :
+                messages.error(request, 'You have already made an inquiry for this property')
+                return redirect('/listings/'+listing_id)
+
+
+        contact = Contact(listing = listing, listing_id=listing_id, name=name,
+        email=email, message=message, user_id=user_id)
+
+        contact.save()
+
+        ## sending mail to inform realtor that was inqiry made for a property
+        send_mail(
+        'Property Listing Inquiry', #email subject
+        'There has been an inquiry fro' + listing + '. Sign into admin panel for more info', # email body
+        'mg69126@gmail.com',# from address wich added in settings , which send from
+        [realtor_email, 'mohamedjamaal745@gmail.com'],#realtor email which send to and cover email
+        fail_silently=False
+        )
+
+        messages.success(request, 'Your request has been submitted, a realtor will get back to you soon . ')
+        return redirect('/listings/'+listing_id)
